9f63c01089a39aaee459382b7e3d8b6c58f08f0c
ZhouLeYao1121 <240187493@stu.vtc.edu.hk>
2026-04-21 16:42:59 +0800

    Add Azure networking resources configuration

---
+++ azure/network.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.cluster_name}-rg"
+  location = var.azure_region
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.cluster_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "private1" {
+  name                 = "private-subnet-1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "private2" {
+  name                 = "private-subnet-2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
