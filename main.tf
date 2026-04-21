e4ab2e8595a045a73ebe2e41b157c81fbef74658
ZhouLeYao1121 <240187493@stu.vtc.edu.hk>
2026-04-21 18:18:41 +0800

    Remove Azure resources and variables for cluster setup
    Removed variables and multiple Azure resources related to the cluster configuration.

--- main.tf
+++ main.tf
@@ -7,60 +7,7 @@ terraform {
   }
 }
 
-variable "azure_region" {
-  type    = string
-  default = "eastasia"
-}
-
-variable "cluster_name" {
-  type    = string
-  default = "itp4121-single"
-}
-
-# Azure 资源组
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.cluster_name}-rg"
-  location = var.azure_region
-}
-
-# Azure VPC网络
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.cluster_name}-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-# Azure 子网1
-resource "azurerm_subnet" "private1" {
-  name                 = "private-subnet-1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Azure 子网2
-resource "azurerm_subnet" "private2" {
-  name                 = "private-subnet-2"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-# Azure AKS Kubernetes集群
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "${var.cluster_name}-aks"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "${var.cluster_name}-aks"
-
-  default_node_pool {
-    name       = "agentpool"
-    node_count = 2
-    vm_size    = "Standard_D2s_v3"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
+  name     = "test-rg"
+  location = "eastasia"
 }
