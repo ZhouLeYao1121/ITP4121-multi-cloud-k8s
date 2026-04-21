b2b784df7fb931b558b8c2f5a924744dbdffb6c7
ZhouLeYao1121 <240187493@stu.vtc.edu.hk>
2026-04-21 16:41:12 +0800

    Add Terraform variables for cloud configurations

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "azure_region" {
+  type    = string
+  default = "eastasia"
+}
+
+variable "gcp_region" {
+  type    = string
+  default = "asia-east1"
+}
+
+variable "gcp_project" {
+  type    = string
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "itp4121-single"
+}
